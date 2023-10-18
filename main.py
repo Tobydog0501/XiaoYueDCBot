b936548644f71077551524bb1a5b2adea561a795
tobydog0501 <tobydog0501@gmail.com>
2023-10-18 19:58:29 +0800

    a

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import asyncio
 from discord.ext import commands
 from dotenv import load_dotenv
 from keep_alive import keep_alive
-from core.core import Logger
+import core.core
+Logger = core.core.Logger
 from discord import app_commands
 MY_GUILD = discord.Object(id=894161476712030218)  # replace with your guild id
 
